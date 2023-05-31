6f26b8b2d2d6de7c1210364fcf794c0ece7e145d
silvialep <silvia.lepore_sl@libero.it>
2023-05-31 17:45:37 +0200

    feat: added types + technologies requests

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,15 +4,36 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index() {
-        $projects = Project::with('type', 'technologies')->paginate(4);
+    public function index(Request $request) {
+
+        $requestData = $request->all();
+        $types = Type::all();
+        $technologies = Technology::all();
+
+        if($request->has('type_id') && $requestData['type_id']) {
+            $projects = Project::where('type_id', $requestData['type_id'])->with('type', 'technologies')->paginate(4);
+
+            if(count($projects) == 0) {
+                return response()->json([
+                    'success' => false,
+                    'error' => 'Non ci sono progetti con questa tipologia'
+                ]);
+            }
+        } else {
+            $projects = Project::with('type', 'technologies')->paginate(4);
+        }
+
         return response()->json([
             'success' => true,
             'results' => $projects,
+            'allTypes' => $types,
+            'allTechnologies' => $technologies,
         ]);
     }
 
